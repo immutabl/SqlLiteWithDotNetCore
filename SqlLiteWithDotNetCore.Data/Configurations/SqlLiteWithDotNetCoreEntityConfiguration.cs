e3b09b166390246267f0d9db681b4c6ba712f8ad
sarin <dev@indrasarkar.com>
2025-12-09 23:05:09 +0000

    Project skeleton setup with EF Core, SQLite and CLEAN-ly architected projects and folders

--- SqlLiteWithDotNetCore.Data/Configurations/SqlLiteWithDotNetCoreEntityConfiguration.cs
+++ SqlLiteWithDotNetCore.Data/Configurations/SqlLiteWithDotNetCoreEntityConfiguration.cs
@@ -1,41 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SqlLiteWithDotNetCore.Domain.Entities;
 
-namespace SqlLiteWithDotNetCore.Data.Configurations
+namespace SqlLiteWithDotNetCore.Data.Configurations;
+
+public sealed class ContactConfiguration : IEntityTypeConfiguration<Contact>
 {
-    internal class SqlLiteWithDotNetCoreEntityConfiguration: IEntityTypeConfiguration<Contact>
+    /*  Configurations here will modify the dbschema
+       this code isn't for validation purposes             */ 
+    public void Configure(EntityTypeBuilder<Contact> builder)
     {
-        /*  Configurations here will modify the dbschema
-            this code isn't for validation purposes             */ 
-        public void Configure(EntityTypeBuilder<Contact> builder)
-        {
-            builder.ToTable("Contact");
+        builder.ToTable("Contact");
+        builder.HasKey(x => x.Id);
 
-            builder.HasKey(x => x.Id);
+        builder.Property(x => x.Forename).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Surname).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Email).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Telno).IsRequired().HasMaxLength(50);
 
-            builder.Property(x => x.Forename)
-                .IsRequired()
-                .HasMaxLength(50);
-
-            builder.Property(x => x.Surname)
-                .IsRequired()
-                .HasMaxLength(50);
-
-            builder.Property(x=> x.Email)
-                .IsRequired()
-                .HasMaxLength(50);
-
-            builder.Property(x => x.Telno)
-                .IsRequired()
-                .HasMaxLength(50);
-        }
-
+        // Optional hardening:
+        // builder.HasIndex(x => x.Email).IsUnique();
     }
 }
